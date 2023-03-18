9601abdb5e4347478c1472fafada7e2b5636a838
Bartol Deak <b@bdeak.net>
2023-03-18 01:38:00 +0100

    add image gen part

--- src/assets-generate.ts
+++ src/assets-generate.ts
@@ -1,39 +1,78 @@
-import fs from "fs";
+import * as fs from "fs";
 import path from "path";
+import _ from "lodash";
 import pLimit from "p-limit";
 import sharp from "sharp";
 
 import { Config } from "./types";
 
-const TRAITS_DIR = "trait-cache";
 const config: Config = JSON.parse(fs.readFileSync("config.json", "utf8"));
+const traitsCache = "trait-cache";
 
-const getTraitImage = (trait: string, value: string) =>
-  path.join(TRAITS_DIR, trait, `${value}.png`);
-
-const optimizeTraitImages = () => {
-  console.log("Optimizing trait images...");
-
-  if (!fs.existsSync(TRAITS_DIR)) {
-    fs.mkdirSync(TRAITS_DIR);
-  }
-
-  const concurrent = pLimit(10);
-
-  for (const trait of config.traits) {
-    const traitDir = path.join(TRAITS_DIR, trait.name);
-    if (!fs.existsSync(traitDir)) {
-      fs.mkdirSync(traitDir);
-    }
-
-    for (const value of trait.values) {
-      concurrent(() =>
-        sharp(value.path)
-          .resize(config.imageSize.width, config.imageSize.height)
-          .toFile(getTraitImage(trait.name, value.name))
-      );
+const mkdir = (...dirs: string[]) => {
+  for (const dir of dirs) {
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir);
     }
   }
 };
 
-optimizeTraitImages();
+const getTraitImage = (trait: string, value: string) =>
+  path.join(traitsCache, trait, `${value}.png`);
+
+const resizeTraitImages = async (): Promise<sharp.OutputInfo[]> => {
+  console.log("Optimizing trait images...");
+
+  mkdir(traitsCache);
+
+  const concurrent = pLimit(10);
+  const promises: Promise<sharp.OutputInfo>[] = [];
+
+  for (const trait of config.traits) {
+    mkdir(path.join(traitsCache, trait.name));
+
+    for (const value of trait.values) {
+      promises.push(
+        concurrent(() =>
+          sharp(value.path)
+            .resize(config.imageSize.width, config.imageSize.height)
+            .toFile(getTraitImage(trait.name, value.name))
+            .finally(() => console.log(`optimized ${trait.name}/${value.name}`))
+        )
+      );
+    }
+  }
+
+  return Promise.all(promises);
+};
+
+const generateAssets = async (): Promise<sharp.OutputInfo[]> => {
+  console.log("Generating assets...");
+
+  mkdir(config.assetsDir);
+
+  const concurrent = pLimit(10);
+  const promises: Promise<sharp.OutputInfo>[] = [];
+
+  for (let i = 0; i < config.count; i++) {
+    const layers = config.traits.map((trait) =>
+      getTraitImage(trait.name, _.sample(trait.values)!.name)
+    );
+
+    promises.push(
+      concurrent(() =>
+        sharp(layers.shift())
+          .composite(layers.map((layer) => ({ input: layer })))
+          .toFile(path.join(config.assetsDir, `${config.offset + i}.png`))
+          .finally(() => console.log(`generated ${config.offset + i}.png`))
+      )
+    );
+  }
+
+  return Promise.all(promises);
+};
+
+(async () => {
+  await resizeTraitImages();
+  await generateAssets();
+})();
